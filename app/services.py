aca752cd4a35128b2528085e0e5b7262582d73fb
Jose Dhonatas <dhontas12alves@gmail.com>
2024-12-10 17:41:30 -0300

    remove unnecessary conditional checks when rounding brent and dollar prices

--- app/services.py
+++ app/services.py
@@ -4,11 +4,11 @@ def get_financial_data():
     try:
         brent = yf.Ticker("BZ=F")
         historico_brent = brent.history(period="1d")
-        brent_price = round(historico_brent["Close"].iloc[-1], 2) if not historico_brent.empty else None
+        brent_price = round(historico_brent["Close"].iloc[-1], 2) 
 
         dolar = yf.Ticker("USDBRL=X")
         historico_dolar = dolar.history(period="1d")
-        dollar_price = round(historico_dolar["Close"].iloc[-1], 2) if not historico_dolar.empty else None
+        dollar_price = round(historico_dolar["Close"].iloc[-1], 2) 
 
         return {
             "brent_price": brent_price,
